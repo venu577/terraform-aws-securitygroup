58e7e3d9086064a5f63253244085334337faf0bb
venu <venubonkuri1234@gmail.com>
2025-07-24 15:42:57 +0530

    adding egress rule

--- main.tf
+++ main.tf
@@ -4,6 +4,15 @@ resource "aws_security_group" "main" {
   description = var.description
   vpc_id      = var.vpc_id
 
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   tags = merge(
     var.sg_tags,
     local.common_tags,
